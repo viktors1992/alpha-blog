3e40ccaca030c50bad0b7df3337f823a7879b168
viktors1992 <v.nilovs@gmail.com>
2022-08-26 14:12:19 +0300

    new record,flash messages,error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,17 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     def new
-        
+       @article = Article.new 
     end
     def create
-        render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article.inspect
+        if @article.save
+            flash[:notice] = "Successfully created"
+            redirect_to @article #article_path(@article) 
+        else 
+            render 'new'
+        end
+        
     end
 end
