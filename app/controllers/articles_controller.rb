aa96ab5e86b0e7ad3e704263fe1db6bd04bd21b6
viktors1992 <v.nilovs@gmail.com>
2022-09-06 22:52:02 +0300

    restrictions, user destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except:[:show, :index]
+    before_action :require_same_user, only:[:edit, :update, :destroy]
     def show   
     end
     def index
@@ -8,8 +10,7 @@ class ArticlesController < ApplicationController
     def new
        @article = Article.new 
     end
-    def edit
-       
+    def edit   
     end
     def create
         @article = Article.new(article_params)
@@ -44,5 +45,11 @@ class ArticlesController < ApplicationController
         end
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+        def require_same_user
+            if current_user != @article.user
+                flash[:alert] = "You can only edit or delete your own article"
+                redirect_to @article
+            end
         end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only:[:edit, :update]
+    before_action :require_same_user, only:[:edit, :update, :destroy]
     def show
         
         
@@ -36,13 +38,29 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "User and all its articles deleted"
+        redirect_to root_path
+        
+    end
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
-private
+
     def set_user
         @user = User.find(params[:id])
     end
+    def require_same_user
+        if current_user != @user
+            flash[:alert] = "You can only edit or delete your own Profile"
+            redirect_to @user
+           
+        end
+    end
+ 
+    
     
 end
