f0b4ed7c0664e8a9536fd45670d16f32af1ca44b
viktors1992 <v.nilovs@gmail.com>
2022-09-06 17:31:30 +0300

    sessions, helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
     end
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         #render plain: @article.inspect
         if @article.save
             flash[:notice] = "Successfully created"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
     def show
         
-        @user = User.find(params[:id])
+        
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     def index
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def update
-        @user = User.find(params[:id])
+       
         if @user.update(user_params)
             flash[:notice] = "Profile updated"
             redirect_to @user
@@ -23,11 +24,12 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
+        
     end
     def create
         @user = User.new(user_params)
         if @user.save 
+            session[:user_id] = @user.id
             flash[:notice] = "Success!!! Welcome #{@user.username}!"
             redirect_to articles_path
         else
@@ -37,6 +39,10 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+private
+    def set_user
+        @user = User.find(params[:id])
     end
     
 end
