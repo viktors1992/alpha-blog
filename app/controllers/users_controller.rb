8b0ef20ce6a6186f44d9967e14ead5783ead59d4
viktors1992 <v.nilovs@gmail.com>
2022-09-06 13:51:35 +0300

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
     def show
+        
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
+    end
+    
     
     def new
         @user = User.new
@@ -11,7 +16,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Profile updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
