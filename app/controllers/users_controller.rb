932678e93c925de541c295fd8a2268eea0014353
viktors1992 <v.nilovs@gmail.com>
2022-09-06 10:44:54 +0300

    add edit template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Profile updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
     def create
         @user = User.new(user_params)
         if @user.save 
