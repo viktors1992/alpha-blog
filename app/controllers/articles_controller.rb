9f5eccf30be344b9792b55c0ee6d77c40b4b7ad0
viktors1992 <v.nilovs@gmail.com>
2022-09-07 17:16:09 +0300

    recovery

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
     def update
         
         if @article.update(article_params)
-            flash[:notice] = "Article updated"
+            flash[:notice] = "Article updated!"
             redirect_to @article
         else
             render 'edit'
