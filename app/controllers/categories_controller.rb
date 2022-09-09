1064c54aa45980328343db62206d69b91dcca1e6
viktors1992 <v.nilovs@gmail.com>
2022-09-09 15:41:58 +0300

    category create action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
     def new
-        
+        @category = Category.new
     end
     def index
         
@@ -8,7 +8,22 @@ class CategoriesController < ApplicationController
     def show
         
     end
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was saved succesfully"
+            redirect_to @category
+        else
+           render 'new' 
+        end
+    end
     
+   private
+   
+   def category_params
+    params.require(:category).permit(:name)
     
+   end
+   
     
 end
