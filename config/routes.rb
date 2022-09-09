1a890e5f86f7b6466d8dcbc4d89879c181e3a3be
viktors1992 <v.nilovs@gmail.com>
2022-09-09 12:59:59 +0300

    categories controller,routes,views

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  #categories
+  resources :categories, except: [:destroy]
 
 end
