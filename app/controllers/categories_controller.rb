819eb7200b79332e3d9b022b3aaa3dab327b6bea
viktors1992 <v.nilovs@gmail.com>
2022-09-11 22:07:59 +0300

    categories list page and navigation update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
     def new
         @category = Category.new
     end
@@ -24,6 +25,10 @@ class CategoriesController < ApplicationController
     params.require(:category).permit(:name)
     
    end
-   
-    
+   def require_admin
+    if !(logged_in? && current_user.admin?)
+        flash[:alert] = "Only admin can perform this action"
+        redirect_to categories_path
+    end
+   end   
 end
